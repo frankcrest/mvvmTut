d64c1da9a368b39c8857863691ceeb15a5e53155
frankcrest <frankcrestview@hotmail.com>
2019-10-23 16:49:46 -0400

    Initial Commit

---
+++ mvvmTut/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  mvvmTut
+//
+//  Created by Frank Chen on 2019-10-23.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue // TODO: Proof we have loaded
+    }
+
+
+}
+
